134def60ed872842e827de3ceca2ee2a5a7b1324
Florian Chaillou <tighter-bandage-5j@icloud.com>
2025-09-26 15:44:03 +0200

    fix: typo in file name

--- tests/test_streamlit.py
+++ tests/test_streamlit.py
@@ -1,5 +1,5 @@
 from streamlit.testing.v1 import AppTest
 
 def test_app_runs():
-    at = AppTest.from_file("../anchorv1.py").run()
+    at = AppTest.from_file("anchor_v1.py").run()
     assert at is not None
